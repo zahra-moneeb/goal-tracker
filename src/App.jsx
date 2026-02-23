a4f69df14c3f4634723a0711a79e32b41861b7ef
Zahra Moneeb <zahramoneeb@gmail.com>
2026-02-23 21:08:50 +0430

    themeProvider added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { ColorModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
 import { Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Goals from "./pages/Goals"
@@ -9,22 +11,34 @@ import NotFound from "./pages/NotFound"
 import MainLayout from "./layout/MainLayout"
 
 function App() {
+  const [theme, colorMode] = useMode();
 
 
   return (
-    <Routes>
-      <Route element={<MainLayout/>}>
-          <Route path="/" element={<Dashboard/>} />
-          <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/goals" element={<Goals/>}/>
-          <Route path="/goals/new" element={<NewGoal/>}/>
-          <Route path="/goals/:id" element={<GoalDetails/>}/>
-          <Route path="/categories" element={<Categories/>}/>
-          <Route path="/settings" element={<Settings/>}/>
-          <Route path="*" element={<NotFound/>}/>
-     </Route>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className="content">
+            <Routes>
+              <Route element={<MainLayout/>}>
+                <Route path="/" element={<Dashboard/>} />
+                <Route path="/dashboard" element={<Dashboard/>} />
+                <Route path="/goals" element={<Goals/>}/>
+                <Route path="/goals/new" element={<NewGoal/>}/>
+                <Route path="/goals/:id" element={<GoalDetails/>}/>
+                <Route path="/categories" element={<Categories/>}/>
+                <Route path="/settings" element={<Settings/>}/>
+              <Route path="*" element={<NotFound/>}/>
+          </Route>
        
     </Routes>
+          </main>
+        </div>
+
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+   
   )
 }
 
